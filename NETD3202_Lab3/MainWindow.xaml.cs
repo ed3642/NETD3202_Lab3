46ba3468072339624c9980437231bad2f429236f
eduardo san martin celi <e.sanmartin99@gmail.com>
2020-11-01 14:49:35 -0500

    Add project files.

---
+++ NETD3202_Lab3/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿/* MainWindow.xaml.cs
+ *      Program Title: IncInc Payroll (messages)
+ * Last Modified Date: Nov 1, 2020
+ *         Written By: Eduardo San Martin Celi
+ * 
+ * Description:
+ * This form formats labs 1 and 2 into tabs and adds the feature of database functionality
+ * and accessing the database data.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NETD3202_Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes the main window
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetDefaults();
+        }
+
+        #region "Event_Hndlers"
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateStatus("Status: load up");
+        }
+
+        /// <summary>
+        /// closes the form
+        /// </summary>
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// resets the color when an message box with an error is changed
+        /// </summary>
+        private void EditedTextbox(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtEdited = (TextBox)sender;
+            txtEdited.Background = Brushes.White;
+            UpdateStatus("Status: error potentially fixed");
+        }
+
+        /// <summary>
+        /// updates the view when the tab is changed
+        /// </summary>
+        private void SwitchTab(object sender, SelectionChangedEventArgs e)
+        {
+            if(tbcPayrollInterface.SelectedIndex == 0)
+            {
+                UpdateStatus("Status: showing payroll entry");
+            }
+            else if (tbcPayrollInterface.SelectedIndex == 1)
+            {
+                // outputing latest info onto the summary page
+                txtEmployeesProcessed.Text = PieceworkWorker.TotalWorkers.ToString();
+                txtAccumulativePay.Text = PieceworkWorker.TotalPay.ToString("c");
+                txtAccumulativeMessages.Text = PieceworkWorker.TotalMessages.ToString();
+                txtAveragePay.Text = PieceworkWorker.AveragePay.ToString("c");
+                UpdateStatus("Status: showing employees summary page");
+            }
+            else if(tbcPayrollInterface.SelectedIndex == 2)
+            {
+                // populates the datagrid
+                dgEmployeeRecords.ItemsSource = PieceworkWorker.AllWorkers.DefaultView;
+                UpdateStatus("Status: showing employee list");
+            }
+        }
+
+        /// <summary>
+        /// Validation for creating a new worker and inserts it into the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // creating object and adding it to the database
+                var myWorker = new PieceworkWorker(txtFName.Text, txtLName.Text, txtTextSent.Text);
+
+                txtTotalPay.Text = myWorker.Pay.ToString("c");
+
+                txtFName.IsEnabled = false;
+                txtLName.IsEnabled = false;
+                txtTextSent.IsEnabled = false;
+                btnCalculate.IsEnabled = false;
+
+                btnClear.Focus();
+
+                UpdateStatus("Status: worker " + txtFName.Text + " " + txtLName.Text + " added with " + txtTextSent.Text + " messages sent");
+            }
+            catch (ArgumentNullException ex)
+            {
+                if (ex.ParamName == "Null Name")
+                {
+                    ExceptionText(txtFName);
+                    lblWorkerFNameError.Content = ex.Message;
+                    UpdateStatus("Status: error");
+                }
+                if (ex.ParamName == "Null Name")
+                {
+                    ExceptionText(txtLName);
+                    lblWorkerLNameError.Content = ex.Message;
+                    UpdateStatus("Status: error");
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                if (ex.ParamName == "Messages Range")
+                {
+                    ExceptionText(txtTextSent);
+                    lblMessagesSentError.Content = ex.Message;
+                    UpdateStatus("Status: error");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                if (ex.ParamName == "Messages Datatype")
+                {
+                    ExceptionText(txtTextSent);
+                    lblMessagesSentError.Content = ex.Message;
+                    UpdateStatus("Status: error");
+                }
+            }
+            catch (Exception)
+            {
+                // exceptions not prepared for
+                MessageBox.Show("Critical Error!", "Something very unexpected happened.");
+            }
+        }
+
+        /// <summary>
+        /// Sets dafaults event
+        /// </summary>
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            SetDefaults();
+            UpdateStatus("Status: cleared input fields");
+        }
+
+        #endregion
+
+        #region "Methods"
+        /// <summary>
+        /// Updates the status bar
+        /// </summary>
+        /// <param name="statusUpdate">The new status</param>
+        private void UpdateStatus(String statusUpdate)
+        {
+            lblStatus.Content = statusUpdate;
+            lblTime.Content = DateTime.Now.ToString();
+        }
+
+        /// <summary>
+        /// returns the form to default state
+        /// </summary>
+        private void SetDefaults()
+        {
+            //entry form defaults
+            txtFName.Focus();
+
+            txtFName.Clear();
+            txtLName.Clear();
+            txtTextSent.Clear();
+            txtTotalPay.Clear();
+
+            txtFName.IsEnabled = true;
+            txtLName.IsEnabled = true;
+            txtTextSent.IsEnabled = true;
+            btnCalculate.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// Changes a textboxes properties
+        /// </summary>
+        /// <param name="txtBox">The textbox to be managed</param>
+        private void ExceptionText(TextBox txtBox)
+        {
+            txtBox.SelectAll();
+            txtBox.Focus();
+            txtBox.Background = Brushes.Red;
+        }
+
+        #endregion
+    }
+}
---
+++ NETD3202_Lab3/PieceworkWorker.cs
@@ -0,0 +1,328 @@
+﻿/* PieceworkWorker.cs
+ * Program Title: IncInc Payroll (text messages)
+ * Last Modified Date: October 31, 2020
+ *         Written By: Eduardo San Martin Celi
+ * 
+ * Description:
+ * This is a class representing individual worker objects. Each stores
+ * their own name, messages sent. The class methods allow
+ * for calculation of the worker's pay and for updating of shared summary
+ * values. Name and messages sent worked are received as strings.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace NETD3202_Lab3
+{
+    class PieceworkWorker
+    {
+        #region "Variable Declarations"
+
+        // Instance variables
+        private int employeeId;
+        private string employeeFirstName;
+        private string employeeLastName;
+        private int employeeMessages;
+        private decimal employeePay;
+        private DateTime employeeEntryDate;
+
+        //private bool isValid = true;
+
+        // Shared class variables
+        //private static int overallNumberOfEmployees = 0;
+        //private static int overallMessages = 0;
+        //private static decimal overallPayroll = 0M;
+        //private static decimal averagePayroll = 0M;
+
+        // constants
+        private int MIN_MESSAGES = 1;
+        private int MAX_MESSAGES = 10000;
+
+        #endregion
+
+        #region "Constructors"
+
+        /// <summary>
+        /// PieceworkWorker constructor: accepts a worker's name and number of
+        /// messages, sets and calculates values as appropriate.
+        /// </summary>
+        /// <param name="nameValue">the worker's name</param>
+        /// <param name="messageValue">a worker's number of messages sent</param>
+        public PieceworkWorker(string nameFValue, string nameLValue, string messagesValue)
+        {
+            // Validate and set the worker's name
+            this.FirstName = nameFValue;
+            // Validate and set the worker's last name
+            this.LastName = nameLValue;
+            // Validate and set the worker's number of messages
+            this.Messages = messagesValue;
+            //current time entryDate
+            this.EntryDate = DateTime.Now;
+            // Calculcate the worker's pay and update all summary values
+            FindPay();
+
+            //insert the worker into the database
+            DataAccess.InsertNewRecord(this);
+        }
+
+        /// <summary>
+        /// HourlyWorker default constructor: empty constructor used strictly for inheritance and instantiation
+        /// </summary>
+        internal PieceworkWorker()
+        {
+
+        }
+
+        #endregion
+
+        #region "Methods"
+
+        /// <summary>
+        /// Currently called in the constructor, the findPay() method is
+        /// used to calculate a worker's pay using their pay rate and hours
+        /// worked. This also updates all summary values.
+        /// </summary>
+        private void FindPay()
+        {
+            //const int Threshold1 = 1;
+            const int Threshold2 = 1000;
+            const int Threshold3 = 2000;
+            const int Threshold4 = 3000;
+            const int Threshold5 = 4000;
+            const decimal rate1 = 0.021M;
+            const decimal rate2 = 0.028M;
+            const decimal rate3 = 0.035M;
+            const decimal rate4 = 0.040M;
+            const decimal rate5 = 0.045M;
+
+            // if - else block for determining the right rate for each employee
+            if (employeeMessages >= Threshold5)
+            {
+                employeePay = Math.Round(employeeMessages * rate5, 2);
+            }
+            else if (employeeMessages >= Threshold4)
+            {
+                employeePay = Math.Round(employeeMessages * rate4, 2);
+            }
+            else if (employeeMessages >= Threshold3)
+            {
+                employeePay = Math.Round(employeeMessages * rate3, 2);
+            }
+            else if (employeeMessages >= Threshold2)
+            {
+                employeePay = Math.Round(employeeMessages * rate2, 2);
+            }
+            else
+            {
+                employeePay = Math.Round(employeeMessages * rate1, 2);
+            }
+            // accumulates the values of each processed employee
+            /*overallNumberOfEmployees++;
+            overallMessages += employeeMessages;
+            overallPayroll += employeePay;*/
+        }
+
+        #endregion
+
+        #region "Properties"
+
+        /// <summary>
+        /// Gets and sets a first worker's name
+        /// </summary>
+        /// <returns>an employee's first name</returns>
+        public string FirstName
+        {
+            get
+            {
+                return employeeFirstName;
+            }
+            set
+            {
+                // Add validation for the worker's name based on the requirements
+                // document
+                // to check if the string is empty
+                if (value == "")
+                {
+                    //MessageBox.Show("Please enter the worker's name.");
+                    //isValid = false;
+                    throw new ArgumentNullException("Null Name", "Please enter the worker's name.");
+                }
+                else
+                {
+                    employeeFirstName = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets a last worker's name
+        /// </summary>
+        /// <returns>an employee's last name</returns>
+        public string LastName
+        {
+            get
+            {
+                return employeeLastName;
+            }
+            set
+            {
+                // Add validation for the worker's name based on the requirements
+                // document
+                // to check if the string is empty
+                if (value == "")
+                {
+                    //MessageBox.Show("Please enter the worker's name.");
+                    //isValid = false;
+                    throw new ArgumentNullException("Null Name", "Please enter the worker's name.");
+                }
+                else
+                {
+                    employeeLastName = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a worker's entry date
+        /// </summary>
+        /// <returns>an employee's entry date</returns>
+        public DateTime EntryDate
+        {
+            get
+            {
+                return employeeEntryDate;
+            }
+            set
+            {
+                employeeEntryDate = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets and sets the number of messages sent by a worker
+        /// </summary>
+        /// <returns>an employee's number of messages</returns>
+        public string Messages
+        {
+            get
+            {
+                return employeeMessages.ToString();
+            }
+            set
+            {
+                // Add validation for the number of messages based on the
+                // requirements document
+                if (!int.TryParse(value, out employeeMessages))
+                {
+                    //MessageBox.Show("Please enter the worker's messages sent as a positive integer value.");
+                    //isValid = false;
+                    throw new ArgumentException("Must be a positive integer value", "Messages Datatype");
+                }
+                else if (Convert.ToInt32(value) < MIN_MESSAGES || Convert.ToInt32(value) > MAX_MESSAGES)
+                {
+                    throw new ArgumentOutOfRangeException("Messages Range", "Must be between" + MIN_MESSAGES + " and " + MAX_MESSAGES + ".");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the worker's pay
+        /// </summary>
+        /// <returns>a worker's pay</returns>
+        public decimal Pay
+        {
+            get
+            {
+                return employeePay;
+            }
+            set
+            {
+                employeePay = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the worker's id
+        /// </summary>
+        /// <returns>a worker's Id</returns>
+        public int Id
+        {
+            get
+            {
+                return employeeId;
+            }
+            set
+            {
+                employeeId = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the overall total pay among all workers
+        /// </summary>
+        /// <returns>the overall total pay among all workers</returns>
+        public static decimal TotalPay
+        {
+            get
+            {
+                return Convert.ToDecimal(DataAccess.GetTotalPay());
+            }
+        }
+
+        /// <summary>
+        /// Gets the overall number of workers
+        /// </summary>
+        /// <returns>the overall number of workers</returns>
+        public static int TotalWorkers
+        {
+            get
+            {
+                return Convert.ToInt32(DataAccess.GetTotalEmployees());
+            }
+        }
+
+        /// <summary>
+        /// Gets the overall number of messages sent
+        /// </summary>
+        /// <returns>the overall number of messages sent</returns>
+        public static int TotalMessages
+        {
+            get
+            {
+                return Convert.ToInt32(DataAccess.GetTotalMessages());
+            }
+        }
+
+        /// <summary>
+        /// Calculates and returns an average pay among all workers
+        /// </summary>
+        /// <returns>the average pay among all workers</returns>
+        public static decimal AveragePay
+        {
+            get
+            {
+                // Write the logic that will return the average pay among all workers. Test this carefully!
+                // avoids deviding by 0
+                return (TotalWorkers == 0) ? 0 : Math.Round(TotalPay / TotalWorkers, 2);
+
+            }
+        }
+
+        /// <summary>
+        /// To populate the datagrid
+        /// </summary>
+        public static DataTable AllWorkers
+        {
+            get
+            {
+                return DataAccess.GetEmployeeList();
+            }
+        }
+
+        #endregion
+
+    }
+}
